7d162576caee6574bd3f95b8bbbe5270923bd689
Damian Dabrowski <dabdamian@gmail.com>
2017-02-14 13:35:23 +0100

    image size validation

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, TextInput
-
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
 
@@ -14,16 +14,6 @@ class ProfileForm(ModelForm):
             "description": "Opis",
             "city": "Miasto",
         }
-
-
-	def clean_image(self):
-		image = self.cleaned_data.get('avatar',False)
-		if image:
-		 if image._size > 1*512*512:
-		       raise ValidationError("Image file too large ( > 0.5mb )")
-		 return image
-		else:
-		 raise ValidationError("Couldn't read uploaded image")
 
 
 class AccountForm(UserChangeForm):
--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 class Profile(models.Model):
+
+	def validate_image(fieldfile_obj):
+		filesize = fieldfile_obj.file.size
+		megabyte_limit = 1
+		if filesize > megabyte_limit*1024*1024:
+			raise ValidationError("Zbyt duży rozmiar pliku. Użyj zdjęcia o rozmiarze poniżej %sMB" % str(megabyte_limit))
+
 	user = models.OneToOneField(User, blank=True, null=True)
-	avatar = models.ImageField(upload_to='avatar', blank=True)
+	avatar = models.ImageField(upload_to='avatar', blank=True, validators=[validate_image])
 	description = models.TextField()
 	city = models.CharField(max_length=100)
 
