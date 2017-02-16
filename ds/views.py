5b5f466433995161fda62239ee9ca15285b0086e
Damian Dabrowski <dabdamian@gmail.com>
2017-02-16 21:30:06 +0100

    better styles css

--- ds/views.py
+++ ds/views.py
@@ -6,12 +6,15 @@ from django.contrib.auth.models import User
 from profiles.models import Profile
 
 def home(request):
+	
 	return render(request, 'home.html')
 
 def about(request):
+	profile = request.user.profile
 	return render(request, 'about.html')
 
 def contact(request):
+	
 	return render(request, 'contact.html')	
 
 @login_required
@@ -19,6 +22,3 @@ def panel(request):
 	profile = request.user.profile
 	return render(request, 'panel.html', {'user': request.user,'profile': profile,})
 
-def base(request):
-	profile = request.user.profile
-	return render(request, 'base.html', {'user': request.user,'profile': profile,})
