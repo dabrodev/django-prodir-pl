bfc372b98055baed456e8f141dcf38874f7be396
Damian Dabrowski <dabdamian@gmail.com>
2017-02-17 21:54:47 +0100

    amazon s3 storage

--- ds/settings.py
+++ ds/settings.py
@@ -72,7 +72,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
-    'profiles'
+    'profiles',
+    'storages',
 ]
 
 SITE_ID = 1
@@ -186,6 +187,44 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+os.environ['S3_USE_SIGV4'] = 'True'
+AWS_S3_HOST = 's3.eu-central-1.amazonaws.com'
+
+
+access_key = 'AKIAJ7PZBS7VSUZ27E3Q'
+secret_key = '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'
+
+AWS_ACCESS_KEY_ID = access_key
+AWS_SECRET_ACCESS_KEY = secret_key
+AWS_STORAGE_BUCKET_NAME = 'zmdpl'
+
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
+
+DEFAULT_FILE_STORAGE = 'ds.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'ds.utils.StaticRootS3BotoStorage'
+
+S3DIRECT_REGION = 'eu-central-1'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+import datetime
+
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = { 
+    'Expires': expires,
+    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+}
+
 
 import dj_database_url
 db_from_env = dj_database_url.config()
