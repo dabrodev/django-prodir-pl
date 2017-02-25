a93a7638935f3e88d98489b9f3b689ae6f9d280e
Damian Dabrowski <dabdamian@gmail.com>
2017-02-25 22:11:25 +0100

    clean

--- profiles/urls.py
+++ profiles/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	url(r'^kontakt-doradca/$', views.message_to_consultant, name='ask_consultant'),
 	url(r'^(?P<pk>\d+)/$', views.profile_detail, name='detail'),
 	url(r'^(?P<pk>\d+)/edit/$', views.edit_profile, name='edit'),
-	url(r'^(?P<pk>\d+)/add_review/$', views.add_review, name='add_review'),
-	url(r'^(?P<pk>\d+)/inquiry/$', views.inquiry, name='inquiry'),
+	url(r'^(?P<pk>\d+)/opinia/$', views.add_review, name='add_review'),
+	url(r'^(?P<pk>\d+)/kontakt-doradca/$', views.inquiry, name='inquiry'),
 	url(r'^\d+/password/$', RedirectView.as_view(pattern_name='account_change_password', permanent=False))
 ]
